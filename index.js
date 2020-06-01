19837c35d0c3fc98e02971ae1ae412ade67f92a9
Don Mallory <dmm333@gmail.com>
2020-06-01 15:08:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code here!
+let element = document.createElement("div");
+
+document.body.appendChild(element);
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = (i+1).toString()
+    ul.appendChild(li)
+}
+
+element.appendChild(ul);
+
+element.style.backgroundColor = 'yellow';
+
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left'
+
+element.className = "dog";
+element.className = "pet-listing dog"
+
+document.querySelector('#main').remove()
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'DON-MALLORY is the champion'
