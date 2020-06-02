42556ac2d52a40943fbf5b38ea4ecc30e6f6fb1b
James <jmcelroy@plexustech.net>
2020-06-02 13:44:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "James is the champion!";
+newHeader.className = "victory";
