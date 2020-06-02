dfea524eb1fd1cbcd420f42c6565290e7398c3ba
tharveybrown <tharveybrown@gmail.com>
2020-06-02 09:20:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.getElementById("main")
+main.remove()
+
+const newHeader  = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Thea is the champion"
+document.body.appendChild(newHeader)
